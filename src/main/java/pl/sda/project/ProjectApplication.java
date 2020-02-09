d0eb1b007723de3691e90d6a8abe3ce024cc36d6
Jakub <kubplon@gmail.com>
2020-02-09 11:34:02 +0100

    secrets in seperate file

--- src/main/java/pl/sda/project/ProjectApplication.java
+++ src/main/java/pl/sda/project/ProjectApplication.java
@@ -2,8 +2,10 @@ package pl.sda.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:secrets.properties")
 public class ProjectApplication {
 
 	public static void main(String[] args) {
